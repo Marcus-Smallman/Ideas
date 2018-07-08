bcee83662cdeb3879032dc503beaf759f8a4f5a7
marcus-smallman <marcussmallman@live.co.uk>
2018-07-08 15:21:07 +0100

    Removed sudo

--- scripts/build/install-prerequisites.sh
+++ scripts/build/install-prerequisites.sh
@@ -1,6 +1,6 @@
 # Update and install packages
-sudo apt-get update
-sudo apt-get install curl
+apt-get update
+apt-get install curl
 
 # Download and install open-faas
-curl -sSL https://cli.openfaas.com | sudo -E sh
+curl -sSL https://cli.openfaas.com | -E sh

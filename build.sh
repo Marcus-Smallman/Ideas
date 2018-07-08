cff8520a2d02f866d63589c68f941885e6c9e064
marcus-smallman <marcussmallman@live.co.uk>
2018-07-08 15:06:52 +0100

    Still woring this travis thing out

--- build.sh
+++ build.sh
@@ -1,9 +1,2 @@
-# Update and install packages
-sudo apt-get update
-sudo apt-get install curl
-
-# Download and install open-faas
-curl -sSL https://cli.openfaas.com | sudo -E sh
-
 # Build function(s)
 faas-cli build -f idea-fn.yml

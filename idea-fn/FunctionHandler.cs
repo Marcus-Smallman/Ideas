6331aa3dcbba25ea5d081768425bc15d9a9e27e6
marcus-smallman <marcussmallman@live.co.uk>
2018-09-02 17:17:51 +0100

    Can now insert to local mongo instance

--- idea-fn/FunctionHandler.cs
+++ idea-fn/FunctionHandler.cs
@@ -8,7 +8,21 @@ namespace Function
     public class FunctionHandler
     {
         public void Handle(string input) {
-            Console.WriteLine("Hi there - your input was: "+ input);
+
+            var client = new MongoClient("mongodb://192.168.0.7:27017");
+            var clientDB = client.GetDatabase("ideasdb");
+            var collection = clientDB.GetCollection<BsonDocument>("ideas");
+            
+            var id = Guid.NewGuid().ToString();
+            var document = new BsonDocument
+            {
+                { "id", id },
+                { "input", BsonValue.Create(input) }
+            };
+
+            collection.InsertOne(document);
+
+            Console.WriteLine(id);
         }
     }
 }

94161e787f2c00125471dbd77a3adcbf488a2fbf
marcus-smallman <marcussmallman@live.co.uk>
2018-11-21 23:04:56 +0000

    Added file static server for vue client app

---
+++ client/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseWebRoot(Path.Combine(Directory.GetCurrentDirectory(), "dist"))
+                .UseStartup<Startup>();
+    }
+}
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,18 +1,18 @@
 // vue.config.js
 module.exports = {
-    configureWebpack: config => {
-        if (process.env.NODE_ENV === 'production') {
-        // mutate config for production...
-        } else {
-        // mutate for development...
-        }
-    },
-    devServer: {
-        proxy: {
-            '/function': {
-                target: 'http://192.168.0.13:31112',
-                changeOrigin: true
-            }
-        }
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // mutate config for production...
+    } else {
+      // mutate for development...
     }
+  },
+  devServer: {
+    proxy: {
+      '/function': {
+        target: 'http://192.168.0.13:31112',
+        changeOrigin: true
+      }
+    }
+  }
 }
